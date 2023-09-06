07151cc7e49a94e1bb8d73f6444659c64470f2df
“ShayM” <shay1mualem@gmail.com>
2023-09-06 11:17:40 +0300

    adding abut me

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -6,13 +6,28 @@ const NavBar = () => {
     console.log('1.1');
   }
   return (
-    <HStack>
-      <Image src={logo} boxSize={'160px'} p={4}></Image>
-      <VStack>
+    <VStack>
+      <HStack>
+        <Image src={logo} boxSize={'160px'} p={4}></Image>
+
         <Text fontSize={'4xl'}>My Portfolio</Text>
-        {/* <Text fontSize={'2xl'}>Here are some of my projects:</Text> */}
-      </VStack>
-    </HStack>
+
+        {/* <Text fontSize={'1xl'}>
+        
+      </Text> */}
+      </HStack>
+      <Text as="i" padding={'25px'}>
+        Hi my name is Shay, I am a motivated and proactive B.Sc. Computer Science.
+        <br /> My passion for learning and staying updated with the latest technologies
+        drives me to excel in the field. I have a solid foundation in computer science and
+        am committed to continuous self-improvement. With expertise in:
+        <br />
+        Java, Spring, GIT, Python, JavaScript, C#, C++, HTML & CSS, SQL & MongoDB,
+        Dockers, Jenkins. I am well-equipped to contribute to innovative projects.
+        <br />I am a versatile professional ready to make a positive impact in the
+        technology field.
+      </Text>
+    </VStack>
   );
 };
 
